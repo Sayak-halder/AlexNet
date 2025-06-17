cbe0a907613b6c616d47f84829ed3e0c0da46fb5
Sayak-halder <haldersayak0@gmail.com>
2025-06-18 00:55:09 +0530

    changes

--- model/model.py
+++ model/model.py
@@ -4,28 +4,28 @@ class AlexNet(nn.Module):
     def __init__(self,num_classes=10):
         super(AlexNet,self).__init__()
         self.features=nn.Sequential(
-            nn.Conv2d(3,64,kernel_size=3,stride=1,padding=1),
+            nn.Conv2d(3,96,kernel_size=11,stride=4,padding=0),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3,stride=2),
+
+            nn.Conv2d(96,256,kernel_size=5,stride=1,padding=2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2,stride=2),
 
-            nn.Conv2d(64,192,kernel_size=3,padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2,stride=2),
-
-            nn.Conv2d(192,384,kernel_size=3,padding=1),
+            nn.Conv2d(256,384,kernel_size=3,stride=1,padding=1),
             nn.ReLU(inplace=True),
 
-            nn.Conv2d(384,256,kernel_size=3,padding=1),
+            nn.Conv2d(384,256,kernel_size=3,stride=1,padding=1),
             nn.ReLU(inplace=True),
 
-            nn.Conv2d(256,256,kernel_size=3,padding=1),
+            nn.Conv2d(384,256,kernel_size=3,stride=1,padding=1),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2,stride=2),
+            nn.MaxPool2d(kernel_size=3,stride=2),
         )
 
         self.classifier=nn.Sequential(
             nn.Dropout(p=0.5),
-            nn.Linear(256*4*4,256*4*4),
+            nn.Linear(256*6*6,256*4*4),
             nn.ReLU(inplace=True),
 
             nn.Dropout(p=0.5),
@@ -37,6 +37,6 @@ class AlexNet(nn.Module):
 
     def forward(self,x):
         x=self.features(x)
-        x=x.view(x.size(0),256*4*4)
+        x=x.view(x.size(0),256*6*6)
         x=self.classifier(x)
         return x
